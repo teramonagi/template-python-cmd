f5fc462c5c150d2c5c38739ec705516896c256a1
Nagi <teramonagi@gmail.com>
2017-06-15 12:52:23 +0900

    Updte

--- template_python_cmd/__main__.py
+++ template_python_cmd/__main__.py
@@ -2,10 +2,12 @@ import sys
 from template_python_cmd import hoge
 from template_python_cmd.moge import noge
 
+
 def main():
     print("Main function is called")
     hoge.hello()
     print(noge.f())
+    print("Main function is finished")
 
 if __name__ == '__main__':
     main()
